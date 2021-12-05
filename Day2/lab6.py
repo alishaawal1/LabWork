0032cee166b71e1baea1a2cfb4916de6c2f297b2
alishaawal1 <aleeshaawall455@gmail.com>
2021-12-05 13:18:35 +0545

    project

--- Day2/lab6.py
+++ Day2/lab6.py
@@ -1,4 +1,4 @@
-#You live 4 miles away from university.The bus drives at 24mph but spends 2 mins at each of the stops on the way.
+#You live 4 miles away from university.The bus drives at 25mph but spends 2 mins at each of the stops on the way.
 #How long will the bus journey take?Alternatively,you could run to the university.
 #You jog the first mile at 7mph;then run the next two at 15mph;before jogging the last at 7mph again.
 #Will this be quicker or slower then the bus?
